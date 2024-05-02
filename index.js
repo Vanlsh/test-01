3a88ef004de459b7aef4724328648eab0ee545f6
Vanlsh <iparan800@ukr.net>
2024-05-02 12:16:35 +0300

    test2

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-var isChromium = window.chrome;
-var winNav = window.navigator;
-var vendorName = winNav.vendor;
-var isOpera = typeof window.opr !== "undefined";
-var isIEedge = winNav.userAgent.indexOf("Edg") > -1;
-var isIOSChrome = winNav.userAgent.match("CriOS");
-
+let isChromium = window.chrome;
+let winNav = window.navigator;
+let vendorName = winNav.vendor;
+let isOpera = typeof window.opr !== "undefined";
+let isIEedge = winNav.userAgent.indexOf("Edg") > -1;
+let isIOSChrome = winNav.userAgent.match("CriOS");
+let usa = winNav.userAgent;
 console.log(isChromium);
 console.log(winNav);
 console.log(vendorName);
 console.log(isOpera);
 console.log(isIEedge);
 console.log(isIOSChrome);
+console.log(usa);
 
-const info = JSON.stringify({ isChromium, winNav, vendorName, isOpera, isIEedge, isIOSChrome }, null, 2);
+const info = JSON.stringify({ isChromium, winNav, vendorName, isOpera, isIEedge, isIOSChrome, usa }, null, 2);
 
 console.log(info);
 
